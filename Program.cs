f3064b4089efdadd75725698e5c1e552c7f3e008
team <saw251211@gmail.com>
2023-02-01 15:48:01 +0700

    workshop2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace workshop2_1 {
+    class Program {
+            static void Main(string[] args) {
+                int capital = 20000;
+                int year = 3; 
+                float rate = 6.25f
+
+                float result =capital * year * rate / 100;
+
+                Console.WriteLine(result);
+
+            }
+    }
+}
