2bf8695000e524f3bd0bfbb430600469a0902b7f
team <saw251211@gmail.com>
2023-02-01 15:50:18 +0700

    workshop2

--- Program.cs
+++ Program.cs
@@ -2,15 +2,14 @@
 
 namespace workshop2_1 {
     class Program {
-            static void Main(string[] args) {
-                int capital = 20000;
-                int year = 3; 
-                float rate = 6.25f
+        static void Main(string[] args) {
+        int capital = 20000;
+        int year = 3; 
+        float rate = 6.25f
 
-                float result =capital * year * rate / 100;
+        float result =capital * year * rate / 100;
 
-                Console.WriteLine(result);
-
-            }
+        Console.WriteLine(result);
+        }
     }
 }
